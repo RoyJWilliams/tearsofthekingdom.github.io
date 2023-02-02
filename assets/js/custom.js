c8ac721db4d1555806da905f5c8f8324600a06e5
RoyJWilliams <108959440+RoyJWilliams@users.noreply.github.com>
2023-02-01 20:27:46 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,7 +12,7 @@
 			
 			$(".countdowntimer").countdown('2023/05/12 06:00', function(event) {
 				$(this).html(event.strftime(
-					'<div class="count"><h2><strong>TLegend of Zelda<br>Tears of the Kingdom</strong></h2>Friday 12th May 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
+					'<div class="count"><h2><strong>Legend of Zelda<br>Tears of the Kingdom</strong></h2>Friday 12th May 2023<br><br><h1><b>%-D Days</h1></b><h4>%-Hh : %-Mm : %Ss</h4>' +
 					''
 				));
 			});
